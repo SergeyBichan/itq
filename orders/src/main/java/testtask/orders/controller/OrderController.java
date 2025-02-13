8c5a7200ffacded30bf240ef6ddd878551743c3e
Sergey Bichan <ugidancer@gmail.com>
2025-02-13 12:09:33 +0300

    Add method to get List of orders by date and total amount

--- orders/src/main/java/testtask/orders/controller/OrderController.java
+++ orders/src/main/java/testtask/orders/controller/OrderController.java
@@ -1,13 +1,17 @@
 package testtask.orders.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import testtask.orders.dto.OrderDto;
 import testtask.orders.dto.OrderDtoForCreateOrder;
 import testtask.orders.entity.Order;
 import testtask.orders.service.OrderService;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -23,14 +27,27 @@ public class OrderController {
     }
 
     @GetMapping("/{id}")
-    public Order getOrderById(@PathVariable("id") Long id) {
-        return orderService.getOrderById(id);
+    public ResponseEntity<OrderDto> getOrderById(@PathVariable Long id) {
+        return new ResponseEntity<>(orderService.getOrderById(id), HttpStatus.OK);
     }
 
     @PostMapping("/create")
     public ResponseEntity<?> createOrder(@RequestBody OrderDtoForCreateOrder orderDto) {
         orderService.createOrder(orderDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteOrder(@PathVariable Long id) {
+        orderService.deleteOrder(id);
+    }
+
+    @GetMapping("/by-date-and-totalamount")
+    public List<OrderDto> getOrdersByDateAndTotalAmount(
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date,
+            @RequestParam BigDecimal totalAmount
+    ) {
+        return orderService.getAllOrdersByDateAndMoreThanTotalAmount(date, totalAmount);
     }
 
 }
