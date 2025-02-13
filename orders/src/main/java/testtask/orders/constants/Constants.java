2a51790cb79443d8f785eeee5f70bf4d0e4dbd29
Sergey Bichan <ugidancer@gmail.com>
2025-02-13 21:32:46 +0300

    add docker-compose and dockerfiles

--- orders/src/main/java/testtask/orders/constants/Constants.java
+++ orders/src/main/java/testtask/orders/constants/Constants.java
@@ -1,5 +1,5 @@
 package testtask.orders.constants;
 
 public class Constants {
-    public static final String URI_FOR_GENERATE_NUMBER = "http://localhost:8081/api/v1/numbers";
+    public static final String URI_FOR_GENERATE_NUMBER = "http://gen:8081/api/v1/numbers";
 }
