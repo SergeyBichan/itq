94bbe00bcf51045a73b585a3af2185680a544c38
Sergey Bichan <ugidancer@gmail.com>
2025-02-13 17:43:25 +0300

    Managment -> management

--- orders/src/main/java/testtask/orders/controller/OrderController.java
+++ orders/src/main/java/testtask/orders/controller/OrderController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@Tag(name = "Order Managment", description = "API for managing orders")
+@Tag(name = "Order Management", description = "API for managing orders")
 @RequestMapping("api/v1/orders")
 public class OrderController {
 
