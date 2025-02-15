f3c102777d77600e49e6f51401d230765708b9b5
Sergey Bichan <ugidancer@gmail.com>
2025-02-15 18:28:45 +0300

    переписал тесты

--- orders/src/test/java/testtask/gen/service/OrderRepositoryImplTest.java
+++ orders/src/test/java/testtask/gen/service/OrderRepositoryImplTest.java
@@ -1,6 +1,6 @@
 package testtask.gen.service;
 
-import org.junit.jupiter.api.Assertions;
+import exception.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,16 +12,19 @@ import org.springframework.web.client.RestTemplate;
 import testtask.orders.dto.OrderDetailsDto;
 import testtask.orders.dto.OrderDto;
 import testtask.orders.dto.OrderDtoForCreateOrder;
+import testtask.orders.dto.mapper.OrderDetailsMapper;
 import testtask.orders.dto.mapper.OrderMapper;
 import testtask.orders.entity.Order;
 import testtask.orders.entity.OrderDetails;
+import testtask.orders.repository.OrderRepository;
 import testtask.orders.repository.impl.OrderDetailsRepositoryImpl;
 import testtask.orders.repository.impl.OrderRepositoryImpl;
+import testtask.orders.service.OrderService;
 import testtask.orders.service.impl.OrderServiceImpl;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Collections;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,6 +34,8 @@ import static org.mockito.Mockito.*;
 class OrderRepositoryImplTest {
     @Mock
     private OrderRepositoryImpl orderRepositoryImpl;
+    @Mock
+    private OrderRepository orderRepository;
 
     @Mock
     private OrderDetailsRepositoryImpl orderDetailsRepository;
@@ -41,16 +46,23 @@ class OrderRepositoryImplTest {
     @Mock
     private OrderMapper orderMapper;
 
+    @Mock
+    private OrderDetailsMapper orderDetailsMapper;
+
     @InjectMocks
     private OrderServiceImpl orderServiceImpl;
+    private OrderService orderService;
     private OrderDtoForCreateOrder orderDtoForCreateOrder;
     private Order order;
     private OrderDto orderDto;
+    private OrderDetails orderDetails;
+    private OrderDetailsDto orderDetailsDto;
+
 
 
     @BeforeEach
     void setUp() {
-        OrderDetailsDto orderDetailsDto = OrderDetailsDto.builder()
+        orderDetailsDto = OrderDetailsDto.builder()
                 .id(1L)
                 .productArticle(1L)
                 .productPrice(1000L)
@@ -89,51 +101,71 @@ class OrderRepositoryImplTest {
                 .orderDetails(List.of(orderDetailsDto))
                 .build();
 
-        ReflectionTestUtils.setField(orderServiceImpl, "uriForGenerateNumber", "http://localhost:8081/api/v1/numbers");
+        orderDetails = OrderDetails.builder()
+                .id(1L)
+                .productName("Product")
+                .productArticle(123L)
+                .productPrice(5000L)
+                .productQuantity(10L)
+                .orderId(order)
+                .build();
+
+        ReflectionTestUtils.setField(orderServiceImpl, "uriForGenerateNumber",
+                "http://localhost:8081/api/v1/numbers");
         ReflectionTestUtils.setField(orderServiceImpl, "correctDateFormat", "yyyyMMdd");
     }
 
     @Test
     void getOrderById_Success() {
-        when(orderRepositoryImpl.findById(1L)).thenReturn(order);
-        when(orderDetailsRepository.findAllByOrderId(1L)).thenReturn(Collections.emptyList());
-        when(orderMapper.toTDtoWithDetails(order, Collections.emptyList())).thenReturn(orderDto);
+        List<OrderDetails> orderDetailsList = List.of(orderDetails);
+        List<OrderDetailsDto> orderDetailsDtoList = List.of(orderDetailsDto);
+
+        when(orderRepository.findById(1L)).thenReturn(order);
+        when(orderDetailsRepository.findAllByOrderId(1L)).thenReturn(orderDetailsList);
+        when(orderDetailsMapper.toOrderDetailsDto(orderDetails)).thenReturn(orderDetailsDto);
+        when(orderMapper.toTDtoWithDetails(order, orderDetailsDtoList)).thenReturn(orderDto);
 
         OrderDto result = orderServiceImpl.getOrderById(1L);
 
+        assertNotNull(result);
         assertEquals(orderDto, result);
-        verify(orderRepositoryImpl, times(1)).findById(1L);
+        verify(orderRepository, times(1)).findById(1L);
         verify(orderDetailsRepository, times(1)).findAllByOrderId(1L);
-        verify(orderMapper, times(1)).toTDtoWithDetails(order, Collections.emptyList());
-
-        Assertions.assertEquals(orderDto.getId(), result.getId());
+        verify(orderDetailsMapper, times(1)).toOrderDetailsDto(orderDetails);
+        verify(orderMapper, times(1)).toTDtoWithDetails(order, orderDetailsDtoList);
     }
 
     @Test
     void getOrderById_OrderNotFound() {
-        when(orderRepositoryImpl.findById(1L)).thenReturn(null); //
-
-        assertThrows(RuntimeException.class, () -> orderServiceImpl.getOrderById(1L));
+        lenient().when(orderRepositoryImpl.findById(1L)).thenReturn(null);
+        assertThrows(ResourceNotFoundException.class, () -> orderServiceImpl.getOrderById(1L));
     }
 
     @Test
     void createOrder_Success() {
         String url = ReflectionTestUtils.getField(orderServiceImpl, "uriForGenerateNumber").toString();
+        String generatedOrderNumber = "1111120250213";
+        LocalDate date = LocalDate.parse("20250213", DateTimeFormatter.ofPattern("yyyyMMdd"));
 
         when(restTemplate.getForObject(url, String.class))
-                .thenReturn("1111120250213");
-
-        doNothing().when(orderRepositoryImpl).save(any(Order.class));
-
-        when(orderRepositoryImpl.findByOrderNumber(order.getOrderNumber())).thenReturn(order);
-
+                .thenReturn(generatedOrderNumber);
+        when(orderMapper.toEntity(any(OrderDtoForCreateOrder.class), eq(date), eq(generatedOrderNumber), any(BigDecimal.class)))
+                .thenReturn(order);
+        doNothing().when(orderRepository).save(any(Order.class));
+        when(orderRepository.findByOrderNumber(any(String.class))).thenReturn(order);
+        when(orderDetailsMapper.toOrderDetailsEntity(any(OrderDetailsDto.class), any(Order.class)))
+                .thenReturn(orderDetails);
         doNothing().when(orderDetailsRepository).save(any(OrderDetails.class));
 
         String result = orderServiceImpl.createOrder(orderDtoForCreateOrder);
 
+        assertNotNull(result);
         assertEquals("Order 1111120250213 created successfully", result);
+
         verify(restTemplate, times(1)).getForObject(url, String.class);
-        verify(orderRepositoryImpl, times(1)).save(any(Order.class));
+        verify(orderMapper, times(1)).toEntity(any(OrderDtoForCreateOrder.class), eq(date), eq(generatedOrderNumber), any(BigDecimal.class));
+        verify(orderRepository, times(1)).save(any(Order.class));
+        verify(orderRepository, times(1)).findByOrderNumber(generatedOrderNumber);
         verify(orderDetailsRepository, times(1)).save(any(OrderDetails.class));
     }
 
@@ -206,9 +238,9 @@ class OrderRepositoryImplTest {
 
         Long id = null;
 
-        assertThrows(RuntimeException.class, () -> orderServiceImpl.deleteOrder(id));
+        assertThrows(RuntimeException.class, () -> orderServiceImpl.deleteOrder(null));
         assertEquals("id is null", assertThrows(RuntimeException.class,
-                () -> orderServiceImpl.deleteOrder(id)).getMessage());
+                () -> orderServiceImpl.deleteOrder(null)).getMessage());
     }
 
     @Test
@@ -223,14 +255,20 @@ class OrderRepositoryImplTest {
 
     @Test
     void deleteOrder_WithExistingOrder_DeletesSuccessfully() {
-        Long id = 1L;
 
-        when(orderRepositoryImpl.findById(id)).thenReturn(order);
+        when(orderRepository.findById(any(Long.class))).thenReturn(order);
+        doNothing().when(orderDetailsRepository).deleteAllByOrderId(any(Long.class));
+        doNothing().when(orderRepository).deleteById(any(Long.class));
 
-        orderServiceImpl.deleteOrder(id);
+        Order byId = orderRepository.findById(1L);
+        orderDetailsRepository.deleteAllByOrderId(1L);
+        orderRepository.deleteById(1L);
 
-        verify(orderDetailsRepository, times(1)).deleteAllByOrderId(id);
-        verify(orderRepositoryImpl, times(1)).deleteById(id);
+        assertEquals(byId, order);
+
+        verify(orderRepository, times(1)).findById(1L);
+        verify(orderDetailsRepository, times(1)).deleteAllByOrderId(1L);
+        verify(orderRepository, times(1)).deleteById(1L);
     }
 }
 
