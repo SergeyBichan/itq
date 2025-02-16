9afc5ad414a84c7bbb629815c4289f0246c0086c
Sergey Bichan <ugidancer@gmail.com>
2025-02-16 10:45:03 +0300

    убрал двойное логирование

--- orders/src/main/java/exception/ResourceNotFoundException.java
+++ orders/src/main/java/exception/ResourceNotFoundException.java
@@ -7,6 +7,5 @@ public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message) {
         super(message);
-        log.error(message);
     }
 }
